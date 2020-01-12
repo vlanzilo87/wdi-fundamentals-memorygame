580bd6896e36020dbcdbcbdeac7c7475bac8e0b4
Vince <vince.lanzilotti@gmail.com>
2020-01-12 13:03:01 -0500

    created game logic

---
+++ memory-game_new/js/main.js
@@ -0,0 +1,14 @@
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("User flipped queen");
+var cardTwo = cards[1];
+cardsInPlay.push('cardTwo');
+console.log("User flipped queen");
+if (cardsInPlay.length === 2)
+	if (cardsInPlay[0] === cardsInPlay[1])
+		alert("You found a match!")
+	else 
+		alert("Sorry, try again.");
+
